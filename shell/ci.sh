068e622da63ba0e33d570c1349e6fad11bfe49c1
Rob LaFeve <roblafeve@gmail.com>
2020-04-06 17:42:04 -0500

    Attempting to use sudo for bash scripts

--- shell/ci.sh
+++ shell/ci.sh
@@ -3,12 +3,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/utils.sh"
 
 print "Clone git submodule..."
-git submodule update --init
+sudo git submodule update --init
 
 
 print "Install app-web dependencies..."
 cd ./consumers/app-web
-npm i
+sudo npm i
 cd ../../
 
 sh "$DIR/web/testGenerators.sh"
--- shell/web/testGenerators.sh
+++ shell/web/testGenerators.sh
@@ -5,36 +5,36 @@ source "$DIR/../utils.sh"
 print "1. Create link for @cremalab/scripts-web..."
 
 cd packages/scripts-web
-npm unlink # Just in case
-npm link
+sudo npm unlink # Just in case
+sudo npm link
 cd ../../
 
 print "2. Link app-web to @cremalab/scripts-web..."
 
 cd consumers/app-web
-npm link @cremalab/scripts-web
+sudo npm link @cremalab/scripts-web
 cd ../../
 
 print "3. Lerna bootstrap..."
 
-npx lerna bootstrap
+sudo lerna bootstrap
 
 print "4. Run Unit Tests..."
 
-npx jest --detectOpenHandles -i
+sudo jest --detectOpenHandles -i
 
 print "5. Teardown links..."
 
 cd consumers/app-web
-rm -rf node_modules/@cremalab/scripts-web
-npm unlink --no-save @cremalab/scripts-web
-npm install @cremalab/scripts-web
+sudo rm -rf node_modules/@cremalab/scripts-web
+sudo npm unlink --no-save @cremalab/scripts-web
+sudo npm install @cremalab/scripts-web
 cd ../../
 
 cd packages/scripts-web
-npm unlink
+sudo npm unlink
 cd ../../
 
 print "6. Show links..."
 
-npm ls -g --depth=0 --link=true
+sudo npm ls -g --depth=0 --link=true
