723577214ad934561425dc4e5362c8b1f3111606
Rob LaFeve <roblafeve@gmail.com>
2020-04-06 18:05:31 -0500

    Add submodules and sudo to top-level action

--- shell/ci.sh
+++ shell/ci.sh
@@ -3,12 +3,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/utils.sh"
 
 print "Clone git submodule..."
-sudo git submodule update --init
+git submodule update --init
 
 
 print "Install app-web dependencies..."
 cd ./consumers/app-web
-sudo npm i
+npm i
 cd ../../
 
-sh "$DIR/web/testGenerators.sh"
+bash "$DIR/web/testGenerators.sh"
--- shell/web/testGenerators.sh
+++ shell/web/testGenerators.sh
@@ -6,39 +6,39 @@ source "$DIR/../utils.sh"
 
 print "1. Create link for @cremalab/scripts-web..."
 
-sudo npm run build
+npm run build
 
 cd packages/scripts-web
-sudo npm unlink # Just in case
-sudo npm link
+npm unlink # Just in case
+npm link
 cd ../../
 
 print "2. Link app-web to @cremalab/scripts-web..."
 
 cd consumers/app-web
-sudo npm link @cremalab/scripts-web
+npm link @cremalab/scripts-web
 cd ../../
 
 print "3. Lerna bootstrap..."
 
-sudo npx lerna bootstrap
+npx lerna bootstrap
 
 print "4. Run Unit Tests..."
 
-sudo npx jest --detectOpenHandles -i
+npx jest --detectOpenHandles -i
 
 print "5. Teardown links..."
 
 cd consumers/app-web
-sudo rm -rf node_modules/@cremalab/scripts-web
-sudo npm unlink --no-save @cremalab/scripts-web
-sudo npm install @cremalab/scripts-web
+rm -rf node_modules/@cremalab/scripts-web
+npm unlink --no-save @cremalab/scripts-web
+npm install @cremalab/scripts-web
 cd ../../
 
 cd packages/scripts-web
-sudo npm unlink
+npm unlink
 cd ../../
 
 print "6. Show links..."
 
-sudo npm ls -g --depth=0 --link=true
+npm ls -g --depth=0 --link=true
