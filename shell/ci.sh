c34b6cd6bda53f99359ef2cc601f319964a7239e
Rob LaFeve <roblafeve@gmail.com>
2020-05-01 15:10:40 -0500

    Remove remote and local shell scripts

--- shell/ci.sh
+++ shell/ci.sh
@@ -2,13 +2,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/utils.sh"
 
-print "Building library..."
+print "Clone git submodule..."
+git submodule update --init
 
-npm run build
+print "Install app-web dependencies..."
+cd ./consumers/app-web
+npm i
+cd ../../
 
-print "Running in CI: $CI"
-
-case $CI in
- true) bash "$DIR/ci.remote.sh" ;;
- *) bash "$DIR/ci.local.sh" ;;
-esac
+bash "$DIR/web/testGenerators.sh"
