ed86e7417ed86571765da0f3b441389e85221e22
Rob LaFeve <roblafeve@gmail.com>
2020-05-01 14:08:42 -0500

    Move CI tests back to scripts

--- shell/utils.sh
+++ shell/utils.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 function print {
-  echo "\033[1;32m \n$1 \033[0m"
+  echo $1
 }
--- shell/web/testGenerators.sh
+++ shell/web/testGenerators.sh
@@ -9,14 +9,14 @@ print "1. Create link for @cremalab/scripts-web..."
 npm run build
 
 cd packages/scripts-web
-sudo npm unlink # Just in case
-sudo npm link
+npm unlink # Just in case
+npm link
 cd ../../
 
 print "2. Link app-web to @cremalab/scripts-web..."
 
 cd consumers/app-web
-sudo npm link @cremalab/scripts-web
+npm link @cremalab/scripts-web
 cd ../../
 
 print "3. Lerna bootstrap..."
@@ -30,13 +30,13 @@ npx jest --detectOpenHandles -i
 print "5. Teardown links..."
 
 cd consumers/app-web
-sudo rm -rf node_modules/@cremalab/scripts-web
-sudo npm unlink --no-save @cremalab/scripts-web
+rm -rf node_modules/@cremalab/scripts-web
+npm unlink --no-save @cremalab/scripts-web
 npm install @cremalab/scripts-web
 cd ../../
 
 cd packages/scripts-web
-sudo npm unlink
+npm unlink
 cd ../../
 
 print "6. Show links..."
