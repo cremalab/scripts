f2ac576eada1a85177f7180266925bf186448695
Rob LaFeve <roblafeve@gmail.com>
2021-02-12 14:08:44 -0600

    Idea 3: nope

--- shell/global/setup-link.sh
+++ shell/global/setup-link.sh
@@ -5,16 +5,16 @@ source "$DIR/../utils.sh"
 print "LINKING: Create @cremalab/scripts-mobile global symlink"
 
 cd packages/scripts-mobile
-sudo npm link --force
+npm link --force
 cd ../../
 
 print "LINKING: Create @cremalab/scripts-web global symlink"
 
 cd packages/scripts-web
-sudo npm link --force
+npm link --force
 cd ../../
 
 print "LINKING: Link packages together"
 print "!!! This is very important"
 
-sudo npm run link
+npm run link
