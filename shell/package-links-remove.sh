20411eeaf405ea7517d9a6acbb96a68dc99d0a3d
Rob LaFeve <roblafeve@gmail.com>
2021-02-13 12:12:42 -0600

    Add package-links-create|remove

---
+++ shell/package-links-remove.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/utils.sh"
+
+print "Removing global package links..."
+
+print "Global links before we start..."
+npm ls -g --depth=0 --link=true
+
+print "Remove global @cremalab/scripts-mobile link..."
+cd packages/scripts-mobile
+npm unlink -g --no-save @cremalab/scripts-mobile
+cd ../../
+
+print "Global links..."
+npm ls -g --depth=0 --link=true
+
+print "Remove global @cremalab/scripts-web link..."
+cd packages/scripts-web
+npm unlink -g --no-save @cremalab/scripts-web
+cd ../../
+
+print "Global links..."
+npm ls -g --depth=0 --link=true
