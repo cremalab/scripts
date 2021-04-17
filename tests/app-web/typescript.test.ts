0b2f6a480d05430b5b50b24ed3a55b8af2a4c7f6
Rob LaFeve <roblafeve@gmail.com>
2021-04-17 10:55:14 -0500

    Change approach to cleanup
    Previously we were using `git` to reset the consumer directories. This
    caused synchronization issues between test runs. We have opted for a more
    direct method of cleanup for each file/directory using `rm -rf` and by
    changing component tests so that each variant uses its own distinct name.

--- tests/app-web/typescript.test.ts
+++ tests/app-web/typescript.test.ts
@@ -1,10 +1,8 @@
-import { cleanup } from "../utils/cleanup"
 import { navigateToConsumer } from "../utils/navigateToConsumer"
 import { validateTypeScript } from "../utils/validateTypeScript"
 
 describe("TypeScript", () => {
   beforeAll(navigateToConsumer("app-web"))
-  afterAll(cleanup)
 
   it("compiles", async () => {
     const result = await validateTypeScript()
