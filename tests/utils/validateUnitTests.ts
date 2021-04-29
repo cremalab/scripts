806867ce1181a6e8929b2bf0872503be1326281c
Rob LaFeve <roblafeve@gmail.com>
2021-04-29 15:43:50 -0500

    GH-160: Add Husky (#161)
    * GH-160: Add husky and husky-add-issue-tracker
    
    * GH-160: Run `ci` script on pre-push
    
    * GH-160: Update submodule
    
    * GH-160: Add end of options delimiter (--)
    
    Duplicates dfc0a4e

--- tests/utils/validateUnitTests.ts
+++ tests/utils/validateUnitTests.ts
@@ -2,7 +2,7 @@ import spawn from "cross-spawn"
 
 export const validateUnitTests = (testPath: string) => {
   return new Promise<string>((resolve) => {
-    const child = spawn("npm", ["run", "test:unit", "--runTestsByPath", "--useStderr", testPath], {
+    const child = spawn("npm", ["run", "test:unit", "--", "--useStderr", "--runTestsByPath", testPath], {
       stdio: "pipe",
       env: { ...process.env, "SKIP_PREFLIGHT_CHECK": "true", CI: "true" }
     })
