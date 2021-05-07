db0a8ca0ae08af0479524eceef3a39bdafb13491
Rob LaFeve <roblafeve@gmail.com>
2021-05-07 17:09:53 -0500

    GH-168: Use HYGEN_TMPLS instead of symlinks for @cremalab/scripts

--- packages/scripts/src/scripts/new.ts
+++ packages/scripts/src/scripts/new.ts
@@ -1,11 +1,8 @@
-import path from "path"
 import spawn from "cross-spawn"
 import { args } from "../utils/args"
-import { pathConsumer, pathPackage } from "../utils/paths"
+import { pathPackageTemplates } from "../utils/paths"
 
-const pathPackageTemplates = path.join(pathPackage(), "_templates")
-const pathConsumerTemplates = path.join(pathConsumer(), "_templates")
-
-spawn.sync("ln", ["-sf", pathPackageTemplates, pathConsumerTemplates])
-spawn.sync("hygen", [args()[0], "new"], { stdio: "inherit" })
-spawn.sync("rm", [pathConsumerTemplates])
+spawn.sync("hygen", [args()[0], "new"], {
+  stdio: "inherit",
+  env: { ...process.env, HYGEN_TMPLS: pathPackageTemplates() },
+})
