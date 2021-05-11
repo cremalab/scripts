2a96fdf47c525bbc82e791bf5c006a58d3d17b42
Rob LaFeve <roblafeve@gmail.com>
2021-05-11 10:59:19 -0500

    GH-168: Use `HYGEN_TMPLS` instead of symlinks (#169)
    * GH-168: Use HYGEN_TMPLS instead of symlinks for @cremalab/scripts
    
    * GH-168: Use HYGEN_TMPLS instead of symlinks for @cremalab/scripts-mobile
    
    * GH-168: Use HYGEN_TMPLS instead of symlinks for @cremalab/scripts-web
    
    * GH-168: Update cremalab/app-web ref
    
    * GH-168: Update cremalab/app-mobile ref

--- packages/scripts-mobile/src/scripts/new.ts
+++ packages/scripts-mobile/src/scripts/new.ts
@@ -1,10 +1,7 @@
-import path from "path"
-import { args, pathConsumer, pathPackage } from "@cremalab/scripts"
+import { args, pathPackageTemplates } from "@cremalab/scripts"
 import spawn from "cross-spawn"
 
-const pathPackageTemplates = path.join(pathPackage(__dirname), "_templates")
-const pathConsumerTemplates = path.join(pathConsumer(), "_templates")
-
-spawn.sync("ln", ["-sf", pathPackageTemplates, pathConsumerTemplates])
-spawn.sync("hygen", [args()[0], "new"], { stdio: "inherit" })
-spawn.sync("rm", [pathConsumerTemplates])
+spawn.sync("hygen", [args()[0], "new"], {
+  stdio: "inherit",
+  env: { ...process.env, HYGEN_TMPLS: pathPackageTemplates(__dirname) },
+})
--- packages/scripts/src/utils/paths.ts
+++ packages/scripts/src/utils/paths.ts
@@ -1,6 +1,10 @@
 import path from "path"
 
 export const pathConsumer = (): string => process.cwd()
+
 export const pathPackage = (d = __dirname): string => {
   return path.relative(pathConsumer(), path.join(d, "../..")) || "./"
 }
+
+export const pathPackageTemplates = (d = __dirname): string =>
+  path.join(pathPackage(d), "_templates")
