642b81d3e53096250015aa1562fe6bebc2cd114f
Rob LaFeve <roblafeve@gmail.com>
2021-05-22 09:23:57 -0500

    GH-180: Add bootstrap to beginning
    Lerna bootstrap installs all package dependencies. Now that `inflection`
    is imported in a TypeScript file, it has uncovered that this dependency
    wasn't ever present up-front but instead was present at the end. Now TS
    requires tht it be installed before building—of course!

--- shell/dev-setup.sh
+++ shell/dev-setup.sh
@@ -2,6 +2,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # GLOBAL - SETUP
+npm run bootstrap
+bash "$DIR/package-links-remove.sh"
 bash "$DIR/global/setup-submodules.sh"
 bash "$DIR/global/setup-build.sh"
 bash "$DIR/package-links-create.sh"
