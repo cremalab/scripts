709b087eed22d2dafd9b92f27d02d9a2797ee80d
Rob LaFeve <roblafeve@gmail.com>
2021-05-26 17:02:16 -0500

    GH-180: Print Node/NPM version at consumer install

--- shell/consumers/mobile/setup-install.sh
+++ shell/consumers/mobile/setup-install.sh
@@ -2,7 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/../../utils.sh"
 
-print "INSTALLING: app-mobile dependencies"
+print "INSTALLING: app-mobile dependencies\nNode: $(node -v) | NPM: $(npm -v)"
 cd ./consumers/app-mobile
 npm ci
 cd ../../
